de01954ea40cda4df2edbfb75606a318cab47f43
Jose Jacsan <jjacsan@gmail.com>
2025-10-01 15:58:52 -0300

    Casting e processamento de dados

--- src/estruturaSequencial/exercicios/ExercicioDeFixacao.java
+++ src/estruturaSequencial/exercicios/ExercicioDeFixacao.java
@@ -1,5 +1,7 @@
 package estruturaSequencial.exercicios;
 
+import java.util.Locale;
+
 public class ExercicioDeFixacao {
     public static void main(String[] args) {
         String product1 = "Computer";
@@ -17,7 +19,11 @@ public class ExercicioDeFixacao {
         System.out.printf("%s, which price is $ %.2f%n", product1, price1);
         System.out.printf("%s, which price is $ %.2f%n", product2, price2);
         System.out.println();
-        
-
+        System.out.printf("Record: %d years old, code %d and gender: %c%n", age, code, gender);
+        System.out.println();
+        System.out.printf("Measue with eight decimal places: %.8f%n", measure);
+        System.out.printf("Rounded (three decimal places): %.3f%n", measure);
+        Locale.setDefault(Locale.US);
+        System.out.printf("US decimal point: %.3f%n", measure);
     }
 }
