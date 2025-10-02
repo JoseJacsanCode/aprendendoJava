add9af2decb17af5db196865af5f06a6e22563f1
Jose Jacsan <jjacsan@gmail.com>
2025-10-02 16:04:24 -0300

    Salvando exercícios

---
+++ src/estruturaSequencial/exercicios/SalarioFuncionario.java
@@ -0,0 +1,52 @@
+package estruturaSequencial.exercicios;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class SalarioFuncionario {
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+        int numFuncionario = sc.nextInt();
+        double horasTrabalhadas = sc.nextDouble();
+        double valorHora = sc.nextDouble();
+
+        double salario = horasTrabalhadas * valorHora;
+
+        System.out.println("NUMBER = " + numFuncionario);
+        System.out.printf("SALARY = U$ %.2f%n", salario);
+
+        sc.close();
+    }
+}
+
+/*
+ *  
+Fazer um programa que leia o número de um funcionário, seu número de horas trabalhadas, o valor que recebe por 
+hora e calcula o salário desse funcionário. A seguir, mostre o número e o salário do funcionário, com duas casas 
+decimais. 
+
+Entrada:
+25 
+100 
+5.50 
+Saída:
+NUMBER = 25 
+SALARY = U$ 550.00 
+
+Entrada:
+1 
+200 
+20.50
+Saída:
+NUMBER = 1 
+SALARY = U$ 4100.00 
+
+Entrada:
+6 
+145 
+15.55
+Saída:
+NUMBER = 6 
+SALARY = U$ 2254.75
+ */
