7a2e894890bd78f1fbcdc8efc8e050b1bc03f943
Jose Jacsan <jjacsan@gmail.com>
2025-10-05 15:16:01 -0300

    Estrutura condicional

---
+++ src/estruturaCondicional/Saudacoes.java
@@ -0,0 +1,16 @@
+package estruturaCondicional;
+
+public class Saudacoes {
+    public static void main(String[] args) {
+        int x = 5;
+        
+        System.out.println("Bom dia");
+        if(x <= 5) {
+            System.out.println("Boa tarde");
+        }
+        System.out.println("Boa noite");
+
+
+
+    }
+}
