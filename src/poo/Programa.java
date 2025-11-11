3bc9ea5e9fca13283f6a61d5e21cb00e6099848c
Jose Jacsan <jjacsan@gmail.com>
2025-11-11 13:16:35 -0300

    Estudando métodos estáticos

---
+++ src/poo/Programa.java
@@ -0,0 +1,36 @@
+package poo;
+
+import java.util.Locale;
+import java.util.Scanner;
+
+public class Programa {
+
+    public static final double PI = 3.14159;
+    public static void main(String[] args) {
+        Locale.setDefault(Locale.US);
+        Scanner sc = new Scanner(System.in);
+
+        System.out.print("Enter radius: ");
+        double radius = sc.nextDouble();
+
+        double c = circumference(radius);
+        double v = volume(radius);
+
+        System.out.printf("Circumference: %.2f%n", c);
+        System.out.printf("Volume: %.2f%n", v);
+        System.out.printf("PI: %.2f%n", PI);
+
+        sc.close();
+    }
+
+    public static double circumference(double radius) {
+        double calcCircumference = 2.0 * PI * radius;
+        return calcCircumference;
+    }
+
+    public static double volume(double radius) {
+        double calcVolume = 4.0 * PI * Math.pow(radius, 3) / 3.0;
+        return calcVolume;
+    }
+
+}
---
+++ src/poo/utilitarios/calculator.java
@@ -0,0 +1,5 @@
+package poo.utilitarios;
+
+public class calculator {
+
+}
