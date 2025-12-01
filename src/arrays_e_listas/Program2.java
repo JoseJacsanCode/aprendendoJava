65e682d91ad2f4a4cf1e33e24e5b6cdd5ecccc19
Jose Jacsan <jjacsan@gmail.com>
2025-11-30 22:28:41 -0300

    Exemplo de wrapper class e classe object

---
+++ src/arrays_e_listas/Program2.java
@@ -0,0 +1,14 @@
+package arrays_e_listas;
+
+public class Program2 {
+    public static void main(String[] args) {
+        int x = 20;
+        Integer obj = x;
+
+        System.out.println(obj);
+
+        int y = obj * 2;
+
+        System.out.println(y);
+    }
+}
