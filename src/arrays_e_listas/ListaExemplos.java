da1e3b54b4dd1069ceaa9db943eb4ffd8450773b
Jose Jacsan <jjacsan@gmail.com>
2025-12-02 14:39:34 -0300

    Exemplos com List

--- src/arrays_e_listas/ListaExemplos.java
+++ src/arrays_e_listas/ListaExemplos.java
@@ -2,6 +2,7 @@ package arrays_e_listas;
 
 import java.util.ArrayList;
 import java.util.List;
+import java.util.stream.Collectors;
 
 public class ListaExemplos {
     public static void main(String[] args) {
@@ -18,5 +19,27 @@ public class ListaExemplos {
             System.out.println(elements);
         }
 
+        list.remove("Anna"); //Compara e remove o elemento entre parênteses
+        System.out.println("---------------------");
+
+        list.removeIf(x -> x.charAt(0) == 'M'); // Remove por predicado
+        for(String elements : list) {
+            System.out.println(elements);
+        }
+
+        System.out.println("---------------------");
+        System.out.println("Index of Bob: " + list.indexOf("Bob")); // Acha o índice do elemento
+        System.out.println("Index of Marco: " + list.indexOf("Marco"));
+        
+        System.out.println("---------------------");
+        List<String> result = list.stream().filter(x -> x.charAt(0) == 'A').collect(Collectors.toList());
+        for(String x : result) {
+            System.out.println(x);
+        }
+        System.out.println("---------------------");
+
+        String name = list.stream().filter(x -> x.charAt(0) == 'J').findFirst().orElse(null) ;
+        System.out.println(name);
+        
     }
 }
