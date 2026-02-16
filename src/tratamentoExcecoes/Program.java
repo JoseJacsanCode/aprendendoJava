8f6cf9b7b51b6b7d40807b461d94e77f02d9774d
Jose Jacsan <jjacsan@gmail.com>
2026-02-16 19:48:46 -0300

    Continuando o estudo sobre exceções

--- src/tratamentoExcecoes/Program.java
+++ src/tratamentoExcecoes/Program.java
@@ -5,22 +5,35 @@ import java.util.Scanner;
 
 public class Program {
     public static void main(String[] args) {
+        
+        method1();
+        System.out.println("End of program");
+    }
+
+    public static void method1() {
+        System.out.println("*****METHOD1 START*****");
+        method2();
+        System.out.println("*****METHOD2 END*****");
+    }
+
+    public static void method2() {
+        System.out.println("*****METHOD2 START*****");
         Scanner sc = new Scanner(System.in);
 
         try {
             String[] vect = sc.nextLine().split(" ");
-            int position = sc.nextInt();
+                int position = sc.nextInt();
             System.out.println(vect[position]);
 
         }
         catch (ArrayIndexOutOfBoundsException e) {
             System.out.println("Invalid position");
+            e.printStackTrace();
         }
         catch(InputMismatchException e) {
             System.out.println("Input error, try again!");
         }
-        System.out.println("End of program");
-
         sc.close();
+        System.out.println("*****METHOD2 END*****");
     }
 }
