059b98da3f72a81d09c7767739c5ac42c66f69a1
Jose Jacsan <jjacsan@gmail.com>
2026-03-17 21:55:50 -0300

    Trabalhando com arquivos

---
+++ src/arquivos/Program3.java
@@ -0,0 +1,23 @@
+package arquivos;
+
+import java.io.BufferedWriter;
+import java.io.FileWriter;
+import java.io.IOException;
+
+public class Program3 {
+    public static void main(String[] args) {
+        String[] lines = new String[] {"Good Morning", "Good afternoon", "Good night"};
+
+        String path = "/home/jjacsan/Workspaces/workspace-java/out.txt";
+
+        try(BufferedWriter bw = new BufferedWriter(new FileWriter(path, true))) {
+            for(String line : lines) {
+                bw.write(line);
+                bw.newLine();
+            }
+        }
+        catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+}
